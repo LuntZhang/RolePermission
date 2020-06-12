8c99727af6521ee826e9bcf4154e5b45cd04a8da
Lunt Zhang <389815117@qq.com>
2020-06-12 10:29:16 +0800

    目录搭建

---
+++ RolePermission/My.Rolepermission.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="My.Rolepermission.WebApp.MvcApplication" Language="C#" %>
---
+++ RolePermission/My.Rolepermission.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(My.Rolepermission.WebApp.Startup))]
+namespace My.Rolepermission.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
